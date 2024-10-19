292d1608ed03dcf0bbd8dff4743cf87fec061476
AmineAkr98 <125382911+AmineAkr98@users.noreply.github.com>
2024-10-20 01:43:40 +0200

    Ajout des fichiers de projet

---
+++ src/main/java/tn/esprit/spring/kaddem/services/EtudiantServiceImpl.java
@@ -0,0 +1,117 @@
+package tn.esprit.spring.kaddem.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import lombok.extern.slf4j.Slf4j;
+
+import tn.esprit.spring.kaddem.entities.Contrat;
+import tn.esprit.spring.kaddem.entities.Departement;
+import tn.esprit.spring.kaddem.entities.Equipe;
+import tn.esprit.spring.kaddem.entities.Etudiant;
+import tn.esprit.spring.kaddem.repositories.ContratRepository;
+import tn.esprit.spring.kaddem.repositories.DepartementRepository;
+import tn.esprit.spring.kaddem.repositories.EquipeRepository;
+import tn.esprit.spring.kaddem.repositories.EtudiantRepository;
+
+import javax.transaction.Transactional;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Set;
+
+@Service
+@Slf4j
+public class EtudiantServiceImpl implements IEtudiantService{
+	@Autowired
+	EtudiantRepository etudiantRepository ;
+	@Autowired
+	ContratRepository contratRepository;
+	@Autowired
+	EquipeRepository equipeRepository;
+    @Autowired
+    DepartementRepository departementRepository;
+	public List<Etudiant> retrieveAllEtudiants(){
+	return (List<Etudiant>) etudiantRepository.findAll();
+	}
+
+	public Etudiant addEtudiant (Etudiant e){
+		return etudiantRepository.save(e);
+	}
+
+	public Etudiant updateEtudiant (Etudiant e){
+		return etudiantRepository.save(e);
+	}
+
+	public Etudiant retrieveEtudiant(Integer  idEtudiant){
+		return etudiantRepository.findById(idEtudiant).get();
+	}
+
+	public void removeEtudiant(Integer idEtudiant){
+	Etudiant e=retrieveEtudiant(idEtudiant);
+	etudiantRepository.delete(e);
+	}
+
+	public void assignEtudiantToDepartement (Integer etudiantId, Integer departementId){
+        Etudiant etudiant = etudiantRepository.findById(etudiantId).orElse(null);
+        Departement departement = departementRepository.findById(departementId).orElse(null);
+        etudiant.setDepartement(departement);
+        etudiantRepository.save(etudiant);
+	}
+	@Transactional
+	public Etudiant addAndAssignEtudiantToEquipeAndContract(Etudiant e, Integer idContrat, Integer idEquipe){
+		Contrat c=contratRepository.findById(idContrat).orElse(null);
+		Equipe eq=equipeRepository.findById(idEquipe).orElse(null);
+		c.setEtudiant(e);
+		eq.getEtudiants().add(e);
+return e;
+	}
+
+	public 	List<Etudiant> getEtudiantsByDepartement (Integer idDepartement){
+return  etudiantRepository.findEtudiantsByDepartement_IdDepart((idDepartement));
+	}
+
+	public List<Etudiant> getEtudiantsWithExpiringContracts() {
+		Date currentDate = new Date();
+		Date expirationDate = new Date(currentDate.getTime() + (30L * 24 * 60 * 60 * 1000)); // 30 jours à partir d'aujourd'hui
+
+		List<Etudiant> etudiantsExpirants = new ArrayList<>();
+		List<Etudiant> etudiants = (List<Etudiant>) etudiantRepository.findAll();
+
+		for (Etudiant etudiant : etudiants) {
+			boolean hasExpiringContract = false;
+			for (Contrat contrat : etudiant.getContrats()) {
+				if (!contrat.getArchive() && contrat.getDateFinContrat().before(expirationDate)) {
+					hasExpiringContract = true;
+					break;
+				}
+			}
+			if (hasExpiringContract) {
+				etudiantsExpirants.add(etudiant);
+			}
+		}
+		return etudiantsExpirants;
+	}
+
+	public double calculateAverageContractDurationForEquipe(Integer idEquipe) {
+		Equipe equipe = equipeRepository.findById(idEquipe).orElse(null);
+		if (equipe == null || equipe.getEtudiants() == null || equipe.getEtudiants().isEmpty()) {
+			return 0.0;
+		}
+
+		long totalDuration = 0;
+		int count = 0;
+
+		for (Etudiant etudiant : equipe.getEtudiants()) {
+			for (Contrat contrat : etudiant.getContrats()) {
+				long durationInDays = (new Date().getTime() - contrat.getDateDebutContrat().getTime()) / (1000 * 60 * 60 * 24);
+				totalDuration += durationInDays;
+				count++;
+			}
+		}
+
+		return count == 0 ? 0.0 : (double) totalDuration / count;
+	}
+
+
+}
